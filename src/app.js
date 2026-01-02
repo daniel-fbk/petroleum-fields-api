e7fc5230338657a9f7751ccfe2d8a7ec22de9f15
Daniel <daniel.borsheim@gmail.com>
2026-01-02 09:37:25 +0100

    chore: add routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ import express from "express";
 import fs from "fs";
 import yaml from "js-yaml";
 import swaggerUi from "swagger-ui-express";
+
 import fieldsRouter from "./routes/fields.routes.js";
+import wellsRouter from "./routes/wells.routes.js";
+import wellTypesRouter from "./routes/wellTypes.routes.js";
+import wellSpecsRouter from "./routes/wellSpecs.routes.js";
+import productionRouter from "./routes/production.routes.js";
+import incidentsRouter from "./routes/incidents.routes.js";
+import fieldMetricsRouter from "./routes/fieldMetrics.routes.js";
+import equipmentRouter from "./routes/equipment.routes.js";
+import equipmentMaintenance from "./routes/equipmentMaintenance.routes.js";
+
 import HttpException from "./utils/HttpException.js";
 import logger from "./utils/logger.js";
 
@@ -11,28 +21,32 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Load Swagger YAML
 const swaggerDocument = yaml.load(fs.readFileSync("./docs/swagger.yml", "utf8"));
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// API root
+// Routes
 app.get("/api", (req, res) => {
   res.status(200).json({
     name: "Petroleum Fields API",
-    version: "1.0.0",
     docs: "/api/docs",
   });
 });
 
-// Routes
 app.use("/api/fields", fieldsRouter);
+app.use("/api/wells", wellsRouter);
+app.use("/api/well-types", wellTypesRouter);
+app.use("/api/well-specs", wellSpecsRouter);
+app.use("/api/production", productionRouter);
+app.use("/api/incidents", incidentsRouter);
+app.use("/api/field-metrics", fieldMetricsRouter);
+app.use("/api/equipment", equipmentRouter);
+app.use("/api/equipment-maintenance", equipmentMaintenance);
 
-// 404 handler
+// Error handling
 app.use((req, res, next) => {
   next(new HttpException(404, "Page not found"));
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   logger.error(err.stack);
   res.status(err.status || 500).json({
